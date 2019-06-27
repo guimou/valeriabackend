841e7e0d3775362cff5a00c82160588609457043
Guillaume Moutier <guillaume.moutier@gmail.com>
2019-06-26 22:18:56 -0400

    Env vars + async call to API

--- src/controllers/listbuckets.controller.ts
+++ src/controllers/listbuckets.controller.ts
@@ -4,29 +4,24 @@ import {Request, RestBindings, get} from '@loopback/rest';
 export class ListbucketsController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
   @get('/listbuckets')
-  listBuckets(): string[] {
+  async listBuckets(): Promise<any> {
     const RGW = require('rgw-admin-client');
     const rgw = new RGW({
-      accessKey: '###',
-      secretKey: '#####',
-      host: '####',
-      protocol: 'http',
-      port: 80,
+      host: process.env.GATEWAY_URL,
+      accessKey: process.env.AWS_ACCESS_KEY_ID,
+      secretKey: process.env.AWS_SECRET_ACCESS_KEY,
+      protocol: process.env.PROTOCOL,
+      port: process.env.PORT,
     });
     const admin = rgw.admin;
-    let value: string[];
-    const that = this;
-    value = [''];
-    admin
+    let value;
+    await admin
       .getBucketInfo({uid: 'daber323'})
       .then(function(bucket: any) {
-        console.log(bucket);
-        that.value = bucket;
-        console.log('ok');
+        value = JSON.stringify(bucket);
       })
       .catch(function(error: any) {
-        //value = JSON.stringify(error);
-        console.log('erreur');
+        value = JSON.stringify(error);
       });
     return value;
   }
